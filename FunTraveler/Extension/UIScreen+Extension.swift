72a1c486e6d4ac81009f2beb1c6a60ddc9fdbee7
sallylin <missikun.tw@gmail.com>
2022-04-09 00:21:03 +0800

    Add Extension of UIScreen

---
+++ FunTraveler/Extension/UIScreen+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIScreen+Extension.swift
+//  FunTraveler
+//
+//  Created by 林翊婷 on 2022/4/8.
+//
+
+import UIKit
+
+extension UIScreen {
+
+    static let width = UIScreen.main.bounds.width
+
+    static let height = UIScreen.main.bounds.height
+}
+
