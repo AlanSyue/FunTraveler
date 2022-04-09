151094b7e7d901fabaabe500e46da45cc83827b5
sallylin <missikun.tw@gmail.com>
2022-04-09 16:57:06 +0800

    Add PlanCardHeader

---
+++ FunTraveler/View/PlanCardHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  PlanCardHeaderView.swift
+//  PlanningCardTest
+//
+//  Created by 林翊婷 on 2022/4/9.
+//
+
+import UIKit
+
+class PlanCardHeaderView: UITableViewHeaderFooterView {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+   
+    @IBOutlet weak var selectionView: SelectionView!
+  
+   
+}
+
